efbb1893876fe7d25b0150650cb4e6d98d5cdfb0
Suraj Singh <surajsingh88597@gmail.com>
2025-05-18 19:03:21 +0530

    Question 9

---
+++ question9.py
@@ -0,0 +1,6 @@
+multiple_numbers = int(input("Enter the number whose multiplication table you want : "))
+
+for i in range(1, 11):
+    print(f"{multiple_numbers} * {i} = {multiple_numbers * i}")
+
+
