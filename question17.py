54cebfb3ffdec3ba6caf329c5c3f39555f9f7ac4
Suraj Singh <surajsingh88597@gmail.com>
2025-05-22 08:50:39 +0530

    Question 17

--- question17.py
+++ question17.py
@@ -2,11 +2,11 @@
 num = int(input("Please enter the number : "))
 num_word = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
 num_str = str(num)
-for i in range(len(num_str)):
+for i in range(len(str(num))):
     if i == len(num_str) - 1:
-        print(num_word[int(num_str[i])], end = " ")
+        print(num_word[int(str(num)[i])], end = " ")
     else:
-        print(num_word[int(num_str[i])], end = " ")
+        print(num_word[int(str(num)[i])], end = " ")
 
 
 
